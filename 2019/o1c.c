7c7334917ba310dea56131ac082114979e5c9308
Martinakraft99 <martinakraft99@gmail.com>
2020-12-08 12:32:58 +0100

    2019_1c_done

---
+++ 2019/o1c.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#define PIN_CHANGE_FLAG GIFR->PCIF0
+
+//define global varible
+int8_t static position;
+
+uint8_t rot_pos_init(){
+    //Set PA1 as input
+    DDRA &= ~(1<<0);
+
+    //Set PA0 as pin change interrupt
+    PCMSK0 |= (1<<0);
+
+    //Enable pin interupt
+    GICR |= (1<<3);
+
+    //Declare and initialize variables
+    position = 0;
+
+
+    return 1;
+}
+//interrupt if GI
+ISR(PIN_CHANGE_FLAG){
+    //Check state og channel B
+    if(PORTA->PORTA1 ==1){
+        position++;
+    }
+    else{
+        position--;
+    }
+}
+
+int8_t get_pos(){
+    return positin;
+}
